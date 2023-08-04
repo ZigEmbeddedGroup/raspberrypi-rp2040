2d6bf5b0d60909dbde4915efacd9169bc9ef9c9f
Matt Knight <mattnite@proton.me>
2023-08-03 21:38:46 -0700

    try this

--- src/hal/pio/assembler/comparison_tests.zig
+++ src/hal/pio/assembler/comparison_tests.zig
@@ -53,7 +53,7 @@ test "pio.comparison.addition" {
 }
 
 test "pio.comparison.apa102" {
-    @setEvalBranchQuota(11000);
+    @setEvalBranchQuota(10000);
     try pio_comparison(@embedFile("comparison_tests/apa102.pio"));
 }
 
@@ -153,7 +153,7 @@ test "pio.comparison.st7789_lcd" {
 }
 
 test "pio.comparison.uart_rx" {
-    @setEvalBranchQuota(10000);
+    @setEvalBranchQuota(11000);
     try pio_comparison(@embedFile("comparison_tests/uart_rx.pio"));
 }
 
