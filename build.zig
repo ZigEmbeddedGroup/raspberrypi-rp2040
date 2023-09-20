a2e3028ab820e172329bc37d2eb783a2cc532f2c
Felix "xq" Queißner <git@random-projects.net>
2023-09-20 10:40:44 +0200

    Documents paths to supported devices.

--- build.zig
+++ build.zig
@@ -1,10 +1,6 @@
 const std = @import("std");
 const comptimePrint = std.fmt.comptimePrint;
 
-////////////////////////////////////////
-//      MicroZig Gen 2 Interface      //
-////////////////////////////////////////
-
 const microzig = @import("root").dependencies.imports.microzig; // HACK: Please import MicroZig always under the name `microzig`. Otherwise the RP2040 module will fail to be properly imported.
 
 fn root() []const u8 {
@@ -12,44 +8,17 @@ fn root() []const u8 {
 }
 const build_root = root();
 
-pub const BootROM = union(enum) {
-    artifact: *std.build.CompileStep, // provide a custom startup code
-    blob: std.build.LazyPath, // just include a binary blob
+////////////////////////////////////////
+//      MicroZig Gen 2 Interface      //
+////////////////////////////////////////
 
-    // Pre-shipped ones:
-    at25sf128a,
-    generic_03h,
-    is25lp080,
-    w25q080,
-    w25x10cl,
-
-    // Use the old stage2 bootloader vendored with MicroZig till 2023-09-13
-    legacy,
-};
-
-const linker_script = .{
-    .source_file = .{ .cwd_relative = build_root ++ "/rp2040.ld" },
-};
-
-const hal = .{
-    .source_file = .{ .cwd_relative = build_root ++ "/src/hal.zig" },
-};
-
-const chip = .{
-    .name = "RP2040",
-    .url = "https://www.raspberrypi.com/products/rp2040/",
-    .cpu = .cortex_m0plus,
-    .source = .{
-        .json = .{ .cwd_relative = build_root ++ "/src/chips/RP2040.json" },
-    },
-    .memory_regions = &.{
-        .{ .kind = .flash, .offset = 0x10000100, .length = (2048 * 1024) - 256 },
-        .{ .kind = .flash, .offset = 0x10000000, .length = 256 },
-        .{ .kind = .ram, .offset = 0x20000000, .length = 256 * 1024 },
-    },
-};
+pub fn build(b: *std.Build) !void {
+    _ = b;
+    //  Dummy func to make package manager happy
+}
 
 pub const chips = struct {
+    // Note: This chip has no flash support defined and requires additional configuration!
     pub const rp2040 = .{
         .preferred_format = .{ .uf2 = .RP2040 },
         .chip = chip,
@@ -130,12 +99,45 @@ pub const boards = struct {
     };
 };
 
-pub fn build(b: *std.Build) !void {
-    _ = b;
-    //  Dummy func to make package manager happy
-}
+pub const BootROM = union(enum) {
+    artifact: *std.build.CompileStep, // provide a custom startup code
+    blob: std.build.LazyPath, // just include a binary blob
 
-fn rp2040_configure(comptime bootrom: BootROM) *const fn (host_build: *std.Build, *microzig.Firmware) void {
+    // Pre-shipped ones:
+    at25sf128a,
+    generic_03h,
+    is25lp080,
+    w25q080,
+    w25x10cl,
+
+    // Use the old stage2 bootloader vendored with MicroZig till 2023-09-13
+    legacy,
+};
+
+const linker_script = .{
+    .source_file = .{ .cwd_relative = build_root ++ "/rp2040.ld" },
+};
+
+const hal = .{
+    .source_file = .{ .cwd_relative = build_root ++ "/src/hal.zig" },
+};
+
+const chip = .{
+    .name = "RP2040",
+    .url = "https://www.raspberrypi.com/products/rp2040/",
+    .cpu = .cortex_m0plus,
+    .source = .{
+        .json = .{ .cwd_relative = build_root ++ "/src/chips/RP2040.json" },
+    },
+    .memory_regions = &.{
+        .{ .kind = .flash, .offset = 0x10000100, .length = (2048 * 1024) - 256 },
+        .{ .kind = .flash, .offset = 0x10000000, .length = 256 },
+        .{ .kind = .ram, .offset = 0x20000000, .length = 256 * 1024 },
+    },
+};
+
+/// Returns a configuration function that will add the provided `BootROM` to the firmware.
+pub fn rp2040_configure(comptime bootrom: BootROM) *const fn (host_build: *std.Build, *microzig.Firmware) void {
     const T = struct {
         fn configure(host_build: *std.Build, fw: *microzig.Firmware) void {
             const bootrom_file = getBootrom(host_build, bootrom);
